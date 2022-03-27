747583a5181935049f92cc7c4287bffea3225388
bosen L <bosenli2013@hotmail.com>
2022-03-27 17:08:35 -0400

    added stock app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+//https://docs.google.com/document/d/1HGKqjjcQWXe7EoiUoX7udq-bije-7a0EPr2OV83TITk/edit
+function App() {
+  return (
+    <div className="App">
+      <h1>stock app</h1>
+    </div>
+  );
+}
+
+export default App;
