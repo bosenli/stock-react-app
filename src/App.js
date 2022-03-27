acba7824d37d59db78f2fdbe37e8ea0ddd26d126
bosen L <bosenli2013@hotmail.com>
2022-03-27 18:42:46 -0400

    added code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './components/Header';
 //https://docs.google.com/document/d/1HGKqjjcQWXe7EoiUoX7udq-bije-7a0EPr2OV83TITk/edit
+
 function App() {
   return (
     <div className="App">
-      <h1>stock app</h1>
+      {/* css styling will only apply to top down */}
+      <Header />  
+      
     </div>
   );
 }
---
+++ src/components/Stock.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function Stock({symbol,companyName,latestPrice,change}){
+    return(
+        <div className="stockFrame">
+            <div className="stock-div1">
+                <h2>{symbol}</h2>
+                <h5>{companyName}</h5>
+            </div>
+            
+            <div className="stock-div2">
+                <h3>{latestPrice}</h3>
+                {/* <h3><span className= {change > 0 ? 'background-green': 'background-red'} >{change}</span></h3> */}
+                <h3><button className= {change > 0 ? 'background-green': 'background-red'} >{change}%</button></h3>
+            </div>  
+        </div>
+    )
+}
+
+export default Stock;
+
+{/* <h3><span className= {((Math.sign(changeVal) === 1) && (Math.sign(changeVal) === 0)) ? 'background-green': 'background-red'} >{change}</span></h3> */}
